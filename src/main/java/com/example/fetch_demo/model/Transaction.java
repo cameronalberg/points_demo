5bc8ae239213b22caef9388e9b547fdd4f599cba
Cameron Alberg <cameronalberg@gmail.com>
2022-10-06 09:23:28 -0500

    added unit tests for Transaction

--- src/main/java/com/example/fetch_demo/model/Transaction.java
+++ src/main/java/com/example/fetch_demo/model/Transaction.java
@@ -1,14 +1,16 @@
 package com.example.fetch_demo.model;
 
-import java.util.Date;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
 import java.util.Locale;
 
-public class Transaction {
+public class Transaction implements Comparable<Transaction>{
     private String payer;
     private int points;
-    private Date timestamp;
+    private Instant timestamp;
 
-    public Transaction(String payer, int points, Date timestamp) throws IllegalArgumentException {
+    public Transaction(String payer, int points, Instant timestamp)
+            throws IllegalArgumentException {
         this.setPayer(payer);
         this.setPoints(points);
         this.setTimestamp(timestamp);
@@ -21,6 +23,10 @@ public class Transaction {
     public void setPayer(String input) {
         if (input == null) {
             throw new IllegalArgumentException("payer cannot be null");
+        }
+        if (!input.matches(".*[a-zA-Z0-9]+.*")) {
+            throw new IllegalArgumentException("payer must contain at least " +
+                    "one alphanumeric character");
         }
         else {
             this.payer = input.toUpperCase(Locale.ROOT);
@@ -40,17 +46,32 @@ public class Transaction {
         }
     }
 
-    public Date getTimestamp() {
+    public Instant getTimestamp() {
         return this.timestamp;
     }
 
-    public void setTimestamp(Date input) {
+    public void setTimestamp(Instant input) {
         if (input == null) {
             throw new IllegalArgumentException("timestamp cannot be null");
         }
         else {
-            this.timestamp = input;
+            this.timestamp = input.truncatedTo(ChronoUnit.SECONDS);
         }
     }
 
+    @Override
+    public int compareTo(Transaction o) {
+        return this.getTimestamp().compareTo(o.getTimestamp());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Transaction)) return false;
+        Transaction compare = (Transaction) o;
+        if (!compare.getPayer().equals(this.getPayer())) return false;
+        if (!(compare.getPoints() == this.getPoints())) return false;
+        if (!(compare.getTimestamp() == this.getTimestamp())) return false;
+        return true;
+    }
 }
--- src/test/java/com/example/fetch_demo/model/TransactionTests.java
+++ src/test/java/com/example/fetch_demo/model/TransactionTests.java
@@ -1,31 +1,40 @@
 package com.example.fetch_demo.model;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 @SpringBootTest
 public class TransactionTests {
+    String payer;
+    int points;
+    Instant time;
+
+    @BeforeEach
+    void setup() {
+        payer = "TEST1";
+        points = 100;
+        time = Instant.now();
+    }
 
     @Test
     void canGetTransactionVariables() {
-        String payer = "TEST1";
-        int points = 100;
-        Date time = new Timestamp(System.currentTimeMillis());
         Transaction transaction = new Transaction(payer, points, time);
         Assertions.assertEquals(payer, transaction.getPayer());
         Assertions.assertEquals(points, transaction.getPoints());
         Assertions.assertEquals(points, transaction.getPoints());
+        System.out.println(transaction.getTimestamp());
     }
 
     @Test
     void DateTimeCannotBeNullOrZero() {
-        String payer = "TEST1";
-        int points = 100;
-        Date time = new Timestamp(System.currentTimeMillis());
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> new Transaction(null, points, time));
         Assertions.assertThrows(IllegalArgumentException.class,
@@ -36,17 +45,45 @@ public class TransactionTests {
 
     @Test
     void payerMustContainAlphanumericChars() {
-
+        Transaction transaction = new Transaction(payer, points, time);
+        Assertions.assertEquals(payer, transaction.getPayer());
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Transaction("", points, time));
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Transaction(" ", points, null));
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Transaction("$!@(#$", 0, time));
     }
 
     @Test
     void payerBecomesCapitalized() {
-        String payer = "test1";
-        int points = 100;
-        Date time = new Timestamp(System.currentTimeMillis());
         Transaction transaction = new Transaction(payer, points, time);
         Assertions.assertNotEquals("test1", transaction.getPayer());
         Assertions.assertEquals("TEST1", transaction.getPayer());
+    }
+
+    @Test
+    void transactionsCompareCorrectly() {
+        Instant time_older = time.minus(1, ChronoUnit.HOURS);
+        Instant time_newer = time.plus(5, ChronoUnit.MINUTES);
+        Transaction t = new Transaction(payer, points, time);
+        Transaction t_same = new Transaction(payer, points, time);
+        Transaction t_older = new Transaction(payer, points, time_older);
+        Transaction t_newer = new Transaction(payer, points, time_newer);
+        Assertions.assertTrue(t.compareTo(t_older) > 0);
+        Assertions.assertTrue(t.compareTo(t_newer) < 0);
+        Assertions.assertEquals(0, t.compareTo(t_same));
+    }
+
+    @Test
+    void checkEquality() {
+        Instant time_older = time.minus(1, ChronoUnit.HOURS);
+        Transaction t = new Transaction(payer, points, time);
+        Transaction t_same = new Transaction(payer, points, time);
+        Transaction t_older = new Transaction(payer, points, time_older);
+        Assertions.assertEquals(t, t_same);
+        Assertions.assertEquals(t, t);
+        Assertions.assertNotEquals(t, t_older);
     }
 
 }

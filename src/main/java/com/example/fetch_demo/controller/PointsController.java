6023249a651fbd0fac1ab398b6ab10455b747026
Cameron Alberg <cameronalberg@gmail.com>
2022-10-07 20:05:16 -0500

    fixed /add endpoint to use POST request

--- src/main/java/com/example/fetch_demo/controller/PointsController.java
+++ src/main/java/com/example/fetch_demo/controller/PointsController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.TypeMismatchException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.web.servlet.error.ErrorController;
@@ -14,13 +14,12 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import java.time.Instant;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@RestController
 public class PointsController implements ErrorController{
-    private TransactionManager manager;
+    private final TransactionManager manager;
 
     @Autowired
     public PointsController() {
@@ -28,7 +27,6 @@ public class PointsController implements ErrorController{
     }
 
     @ExceptionHandler(TypeMismatchException.class)
-    @ResponseBody
     public ResponseEntity<Object> handleTypeMismatchException() {
         return new ResponseEntity<>("type mismatch in request parameters. " +
                 "refer to API documentation for correct syntax.",
@@ -36,38 +34,46 @@ public class PointsController implements ErrorController{
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
-    @ResponseBody
     public ResponseEntity<Object> handleMissingRequestParamException() {
         return new ResponseEntity<>("required request parameter not found." +
                 "refer to API documentation for correct syntax.",
                 HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Object> handleUnreadableHTTPMessageException() {
+        return new ResponseEntity<>("message could not be parsed. please " +
+                "refer to API documentation for correct syntax",
+                HttpStatus.BAD_REQUEST);
+    }
+
     @RequestMapping(path="/")
     public String root() {
+        return "redirect:usage";
+    }
+
+    @RequestMapping(path="/usage")
+    public String usage() {
         return "placeholder for API docs.";
     }
 
-    @RequestMapping(path = "/add")
-    public ResponseEntity<Object> addTransaction(@RequestParam String payer,
-                                                 @RequestParam int points,
-                                                 @RequestParam String timestamp) {
-        Transaction t;
-        try {
-            t = new Transaction(payer, points, Instant.parse(timestamp));
-        } catch (DateTimeParseException e) {
-            return new ResponseEntity<>("timestamp could not be parsed.",
-                    HttpStatus.BAD_REQUEST);
-        } catch (IllegalArgumentException e) {
-            return new ResponseEntity<>(e.getMessage(),
-                    HttpStatus.BAD_REQUEST);
-        }
+    @PostMapping(path = "/add")
+    public ResponseEntity<Object> addTransaction(@RequestBody Transaction t) {
+
+//        try {
+//            t = new Transaction(payer, points, Instant.parse(timestamp));
+//        } catch (DateTimeParseException e) {
+//            return new ResponseEntity<>("timestamp could not be parsed.",
+//                    HttpStatus.BAD_REQUEST);
+//        } catch (IllegalArgumentException e) {
+//            return new ResponseEntity<>(e.getMessage(),
+//                    HttpStatus.BAD_REQUEST);
+//        }
         manager.addTransaction(t);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @RequestMapping(path = "/spend")
-    @ResponseBody
     public ResponseEntity<Object> spendPoints(@RequestParam int points) {
         if (!TransactionManager.validatePoints(points)) {
             return new ResponseEntity<>("points must be >= 0",
